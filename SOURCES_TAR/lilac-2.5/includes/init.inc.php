4a0d04eb91712d4e9ba76258ce3facf81c10a813
Eric Belhomme <ebelhomme@ricozome.net>
2019-03-21 09:37:53 +0100

    rgmweb

--- SOURCES_TAR/lilac-2.5/includes/init.inc.php
+++ SOURCES_TAR/lilac-2.5/includes/init.inc.php
@@ -25,7 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 define("LILAC_VERSION", "2.5");
-define("LILAC_NAME", "Eonweb Configurator");
+define("LILAC_NAME", "RGMweb Configurator");
 define("LILAC_FS_ROOT", dirname(__FILE__) . "/../");
 
 // Setup include path to initially include from our library directory before including anything else.
