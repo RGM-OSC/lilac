97544328e3019af8386b3e0d86a1dc8c2003b691
Alexandre ROCHER <arocher@fr.scc.com>
2023-12-19 10:57:49 +0100

    Fix (EL9) php 8 compatibility

--- SOURCES_TAR/lilac/library/propel/connection/PropelPDO.php
+++ SOURCES_TAR/lilac/library/propel/connection/PropelPDO.php
@@ -214,7 +214,7 @@ class PropelPDO extends PDO
 	 *
 	 * @return    boolean
 	 */
-	public function beginTransaction()
+	public function beginTransaction(): bool
 	{
 		$return = true;
 		if (!$this->nestedTransactionCount) {
@@ -235,7 +235,7 @@ class PropelPDO extends PDO
 	 *
 	 * @return    boolean
 	 */
-	public function commit()
+	public function commit(): bool
 	{
 		$return = true;
 		$opcount = $this->nestedTransactionCount;
@@ -264,7 +264,7 @@ class PropelPDO extends PDO
 	 *
 	 * @return    boolean  Whether operation was successful.
 	 */
-	public function rollBack()
+	public function rollBack(): bool
 	{
 		$return = true;
 		$opcount = $this->nestedTransactionCount;
@@ -320,14 +320,14 @@ class PropelPDO extends PDO
 	 * @param     integer  $attribute  The attribute to set (e.g. PropelPDO::PROPEL_ATTR_CACHE_PREPARES).
 	 * @param     mixed    $value  The attribute value.
 	 */
-	public function setAttribute($attribute, $value)
+	public function setAttribute($attribute, $value): bool
 	{
 		switch($attribute) {
 			case self::PROPEL_ATTR_CACHE_PREPARES:
 				$this->cachePreparedStatements = $value;
 				break;
 			default:
-				parent::setAttribute($attribute, $value);
+				return parent::setAttribute($attribute, $value);
 		}
 	}
 
@@ -363,7 +363,7 @@ class PropelPDO extends PDO
 	 *
 	 * @return    PDOStatement
 	 */
-	public function prepare($sql, $driver_options = array())
+	public function prepare($sql, $driver_options = array()): PDOStatement|false
 	{
 		if ($this->useDebug) {
 			$debug = $this->getDebugSnapshot();
@@ -394,7 +394,7 @@ class PropelPDO extends PDO
 	 * @param     string  $sql
 	 * @return    integer
 	 */
-	public function exec($sql)
+	public function exec($sql): int|false
 	{
 		if ($this->useDebug) {
 			$debug = $this->getDebugSnapshot();
@@ -421,7 +421,8 @@ class PropelPDO extends PDO
 	 *
 	 * @return    PDOStatement
 	 */
-	public function query()
+	/*
+	public function query(string $query, ?int $fetchMode = null): PDOStatement|false
 	{
 		if ($this->useDebug) {
 			$debug = $this->getDebugSnapshot();
@@ -442,7 +443,7 @@ class PropelPDO extends PDO
 		}
 
 		return $return;
-	}
+	}*/
 
 	/**
 	 * Clears any stored prepared statements for this connection.
